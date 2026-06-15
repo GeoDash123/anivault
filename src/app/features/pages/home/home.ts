0758530cf944bcf5d100c729b0a6c5c560f7d72d
GeoDash123 <josealatorre233@gmail.com>
2026-06-15 15:58:13 -0700

    Explore reemplazado por Home

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AnimeService } from '../../../core/services/anime.service';
+import { AnimeCard } from '../../../shared/components/anime-card/anime-card'
+import { FormsModule } from '@angular/forms';
+import { Loading } from '../../../shared/components/loading/loading';
+import { ErrorMessage } from '../../../shared/components/error-message/error-message';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, AnimeCard, FormsModule, Loading, ErrorMessage],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  animes: any[] = [];
+  loading = false;
+  error = '';
+
+  constructor(
+    private animeService: AnimeService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.loadTopAnime();
+  }
+
+  loadTopAnime(): void {
+    this.loading = true;
+    this.error = '';
+
+    this.animeService.getTopAnime().subscribe({
+      next: (response: any) => {
+        this.animes = response.data;
+        this.loading = false;
+
+        console.log('Animes cargados:', this.animes);
+
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+        this.error = 'No se pudo cargar la información de anime.';
+        this.loading = false;
+
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  searchText = '';
+
+  searchAnime(): void {
+    if (!this.searchText.trim()) {
+      this.loadTopAnime();
+      return;
+    }
+
+    this.loading = true;
+    this.error = '';
+
+    this.animeService.searchAnime(this.searchText).subscribe({
+      next: (response: any) => {
+        this.animes = response.data;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.error = 'No se pudo realizar la búsqueda.';
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+}
