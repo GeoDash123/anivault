105e52e6830242c73eab9a13f50dbd84e3039128
GeoDash123 <josealatorre233@gmail.com>
2026-06-15 23:26:40 -0700

    Sistema de Favoritos mejorado

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router } from '@angular/router';
-import { ThemeService } from '../../../core/services/theme.service';
-import { AuthService } from '../../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  showLogoutModal = false;
+
   constructor(
     public authService: AuthService,
     public themeService: ThemeService,
     private router: Router
   ) {}
 
-  logout(): void {
+  openLogoutModal(): void {
+    this.showLogoutModal = true;
+  }
+
+  cancelLogout(): void {
+    this.showLogoutModal = false;
+  }
+
+  confirmLogout(): void {
     this.authService.logout();
+    this.showLogoutModal = false;
     this.router.navigate(['/login']);
   }
 }
